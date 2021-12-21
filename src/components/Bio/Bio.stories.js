f6d994a71ac116c4af2a2ba7e58a7c3a4cd9d28b
dhopz <david_houpapa@hotmail.com>
2021-12-21 17:00:30 +0000

    creating bio

---
+++ src/components/Bio/Bio.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bio from './Bio';
+
+export default {
+  title: 'Components/Bio',
+  component: Bio,
+};
+
+const Template = () => {
+  return <Bio/>
+}
+
+export const Default = Template.bind({});
