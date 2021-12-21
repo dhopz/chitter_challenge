2e6500209cb80b1d17eebec1ecb269c2058be431
dhopz <david_houpapa@hotmail.com>
2021-12-21 15:50:25 +0000

    postform added to storybook

---
+++ src/components/ PostForm/PostForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './PostForm.module.scss'
+
+const PostForm = () => {
+    return (
+        <form>
+          <textarea className={styles.formContent}></textarea>
+            <button className={styles.formButton}>
+              Add New Post
+            </button>          
+        </form>
+    )
+}
+
+export default PostForm
---
+++ src/components/ PostForm/PostForm.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostForm from './PostForm';
+
+export default {
+  title: 'Components/PostForm',
+  component: PostForm,
+};
+
+const Template = () => {
+  return <PostForm/>
+}
+
+export const Default = Template.bind({});
