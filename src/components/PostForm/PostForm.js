686def16f20271e3c5847aa0908c837f983ac6f2
dhopz <david_houpapa@hotmail.com>
2022-01-04 16:16:20 +0000

    firebase addDoc for Chitter Posts

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import styles from './PostForm.module.scss'
+import {db} from './firebase'
+import {collection, addDoc, Timestamp} from 'firebase/firestore'
 
 const PostForm = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await addDoc(collection(db, 'posts'), {
+        description: description,
+        completed: false,
+        created: Timestamp.now()
+      })
+      onClose()
+    } catch (err) {
+      alert(err)
+    }
+  }
     return (
         <form>
           <textarea className={styles.formContent}></textarea>
-            <button className={styles.formButton}>
+            <button className={styles.formButton} onClick={handleSubmit}>
               Add New Post
             </button>          
         </form>
